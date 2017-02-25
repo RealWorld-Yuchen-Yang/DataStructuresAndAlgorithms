678c9b41a15524676c5ae7dea95feb8c6707fb24
Yuchen Yang <realworldyucheny@gmail.com>
2017-02-25 08:03:24 -0500

    there are two approaches, approach of using iteratively merge two lists is implemented

---
+++ src/main/sorting/mergesort/MergeKSortedLists.java
@@ -0,0 +1,46 @@
+package main.sorting.mergesort;
+
+import java.awt.List;
+
+import main.sorting.ListNode;
+
+/**
+ * Requirement: 
+ * 		Merge k sorted linked lists and return one sorted list. 
+ * 
+ * Analysis: 
+ * 		Approach1: 
+ * 			Can iteratively use MergeTwoSortedList.merge()
+ * 			Time Complexity: O(n1+n2+..)
+ * 			Space Complexity: O(1)
+ * 		Approach2: 
+ * 			Can parallelly merge the lists in lists array
+ * 			Time Complexity: O(max(n1, n2, ...)
+ * 			Space Complexity: O(lists.length)
+ * @author yuchenyang
+ *
+ */
+public class MergeKSortedLists
+{
+	public static ListNode mergeKLists(ListNode[] lists)
+	{
+		if(lists.length == 0)
+		{
+			return null;
+		}
+		else if(lists.length == 1)
+		{
+			return lists[0];
+		}
+		else
+		{
+			ListNode p = lists[0];
+			for(int i = 1; i<lists.length; i++)
+			{
+				p = MergeTwoSortedLists.mergeTwoLists(p, lists[i]);
+			}
+			return p;
+		}
+	}
+	
+}
