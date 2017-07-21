9b3d8ed276d9a2aea3dac82c05a0b5a255cea275
Yuchen Yang <realworldyucheny@gmail.com>
2017-07-21 12:27:31 -0400

    fix

--- src/main/strings/LongestPalindromicSubstring.java
+++ src/main/strings/LongestPalindromicSubstring.java
@@ -1,32 +1,32 @@
 package main.strings;
 
 /**
- * Problem: 
+ * Problem:
  * 	Given a string s, find the longest palindromic substring in s
- * 	You may assume that the maximum length of s is 1000, 
+ * 	You may assume that the maximum length of s is 1000,
  * 	and there exists one unique longest palindromic substring
- * 
- * Analysis: 
- * 	Since the palindromic string has recursive nature, 
- * 	we can use dynamic programming to solve this problem. 
- * 
+ *
+ * Analysis:
+ * 	Since the palindromic string has recursive nature,
+ * 	we can use dynamic programming to solve this problem.
+ *
  * 	step1: define f(i,j) as the longest palindromic between s's index i and j
- * 	step2: regularity experssions 
- * 		f(i,j) = 
- * 			case1: when i == j, 
+ * 	step2: regularity experssions
+ * 		f(i,j) =
+ * 			case1: when i == j,
  * 				   there is only 1 character in this case, then s[i] or s[j],
  *                 base case
- *                 
- * 			case2: when j = i+1, 
- *                 there are only 2 characters in this case, 
- *                 palindrome exists only if two endpoitns s[i] == s[j],  
+ *
+ * 			case2: when j = i+1,
+ *                 there are only 2 characters in this case,
+ *                 palindrome exists only if two endpoitns s[i] == s[j],
  *                 base case
- * 
- * 			case3: when j > i+1, 
- * 				   there are more than 2 characters in this case,  	
- * 				   recursive definition: 
- * 				   palindrome exists only if two end points s[i] == s[j], 
- * 				   and the middle substring's palindrome is f(i+1, j-1) 
+ *
+ * 			case3: when j > i+1,
+ * 				   there are more than 2 characters in this case,
+ * 				   recursive definition:
+ * 				   palindrome exists only if two end points s[i] == s[j],
+ * 				   and the middle substring's palindrome is f(i+1, j-1)
  * @author yuchenyang
  *
  */
@@ -37,17 +37,17 @@ public class LongestPalindromicSubstring
 		final int n = s.length();
 		final boolean[][] f = new boolean[n][n];
 		int maxLen = 1, start = 0;
-		
+
 		for(int j=0; j<n; j++)
 		{
 			f[j][j] = true;
-			for(int i= 0; i<j; i++)
+			for(int i= 0; i<=j; i++)
 			{
 				f[i][j] = ((s.charAt(i) == s.charAt(j)) &&
 						(j-i<2 || f[j+1][i-1]));
-				
+
 				/*
-				 * if the current examing substring is palindrome 
+				 * if the current examing substring is palindrome
 				 * and its length is greater than the known max
 				 */
 				if(f[i][j] && maxLen<j-i+1)
@@ -57,7 +57,7 @@ public class LongestPalindromicSubstring
 				}
 			}
 		}
-		
+
 		return s.substring(start, start+maxLen);
 	}
 }
