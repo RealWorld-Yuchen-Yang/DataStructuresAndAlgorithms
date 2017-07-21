65460530aa99570ca1175343dfa05756ffe39959
Yuchen Yang <realworldyucheny@gmail.com>
2017-07-21 12:48:18 -0400

    fix bug

--- src/main/strings/WordPattern.java
+++ src/main/strings/WordPattern.java
@@ -5,21 +5,21 @@ import java.util.Map;
 
 /**
  * Problem
- * 	Given a pattern and a string str, find if str follows the same pattern 
+ * 	Given a pattern and a string str, find if str follows the same pattern
  * 	Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str
- * 
+ *
  * Example:
  * 	1. pattern = "abba", str = "dog cat cat dog", should return true;
  * 	2. pattern = "abba", str = "dog cat cat fish", should return false;
  * 	3. pattern = "aaaa", str = "dog cat cat dog", should return false;
  * 	4. pattern = "abba", str = "dog dog dog dog", should return false;
- * 
- * Note: 
- * 	You may assume pattern contains only lowercase letters, 
- * 	and str contains lowercase letters separated by a single space. 
- * 
- * Analysis: 
- * 	This problem is similar to Isomorphic Strings, 
+ *
+ * Note:
+ * 	You may assume pattern contains only lowercase letters,
+ * 	and str contains lowercase letters separated by a single space.
+ *
+ * Analysis:
+ * 	This problem is similar to Isomorphic Strings,
  * 	use two HashMap to store the bijection
  * @author yuchenyang
  *
@@ -33,11 +33,11 @@ public class WordPattern
 		{
 			return false;
 		}
-		
+
 		final Map<Character, String> map1 = new HashMap();
 		final Map<String, Character> map2 = new HashMap();
-		
-		
+
+
 		for(int i = 0; i<words.length; i++)
 		{
 			final Character key1 = pattern.charAt(i);
@@ -48,12 +48,12 @@ public class WordPattern
 				{
 					return false;
 				}
-				else
-				{
-					map1.put(key1, words[i]);
-				}
 			}
-			
+			else
+			{
+				map1.put(key1, words[i]);
+			}
+
 			final String key2 = words[i];
 			if(map2.containsKey(key2))
 			{
@@ -62,13 +62,13 @@ public class WordPattern
 				{
 					return false;
 				}
-				else
-				{
-					map2.put(key2, pattern.charAt(i));
-				}
+			}
+			else
+			{
+				map2.put(key2, pattern.charAt(i));
 			}
 		}
-		
+
 		return true;
 	}
 }
