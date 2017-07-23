106de2bc6ab8b75fe7ad7ac4559e39655280ae56
Yuchen Yang <realworldyucheny@gmail.com>
2017-07-23 10:28:19 -0400

    adding comments

--- src/main/linearlist/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/linearlist/RemoveDuplicatesFromSortedArrayII.java
@@ -2,12 +2,12 @@ package main.linearlist;
 
 /**
  * What if duplicates are allowed at most twice?
- * 
- * Analysis: 
+ *
+ * Analysis:
  * Since the given array is sorted,
  * we can directly compare the current item with the items 2 step from it
- * since the middle item either equal or greater than it. 
- * and will not affect final result 
+ * since the middle item either equal or greater than it.
+ * and will not affect final result
  * @author yuchenyang
  *
  */
@@ -20,12 +20,16 @@ public class RemoveDuplicatesFromSortedArrayII
 		{
 			return nums.length;
 		}
-		
+
 		int index = 2;
 		for(int i=2; i<nums.length; i++)
 		{
 			if(nums[i] != nums[index-2])
 			{
+				/*
+				 * this is an acceptance test,
+				 * only if the current situation satisfies the requirement will we accept the current element
+				 * and move on to the next index
 				nums[index++]=nums[i];
 			}
 		}
