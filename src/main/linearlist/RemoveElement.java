2c4546944554f2bee04979ea8e1c8d2a3a88ec13
Yuchen Yang <realworldyucheny@gmail.com>
2017-07-23 11:00:32 -0400

    fix bug, add comments

--- src/main/linearlist/RemoveElement.java
+++ src/main/linearlist/RemoveElement.java
@@ -1,9 +1,15 @@
 package main.linearlist;
 
 /**
- * Given an array and a value, remove all instances of that value in place and return the new length. 
- * The order of elements can be changed. It doesn't matter what you leave beyond the new length. 
- * 
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ * Note:
+ *  just for information's sake
+ *  you can not resize an array,  you can only create a new array and copy over a subsection of the previous value
+ *  you can shrink an ArrayList,
+ *   syntax: arrayList.subList(startIndex, endIndex).clear()
+ *
  * @author yuchenyang
  *
  */
@@ -17,9 +23,11 @@ public class RemoveElement
 		{
 			/*
 			 * indexPointer moves slower than iPointer,
-			 * it stores values do not equal to target 
+			 * it stores values do not equal to target
 			 */
-			nums[index++]=nums[i];
+			 if (nums[i] != target) {
+				 	nums[index++]=nums[i];
+				}
 		}
 		return index;
 	}
